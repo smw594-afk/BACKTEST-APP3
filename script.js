44e006464fd18b582b2c9f7cfaabab9d38118063
smw594-afk <smw594@gmail.com>
2026-06-24 07:34:22 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js (UI 컨트롤, 데이터 통신 및 차트 렌더링 - 6슬롯 무한 확장 버전)
 
-const APP_VERSION = "3.38";
+const APP_VERSION = "3.391";
 const MAX_SLOTS = 6;
 
 function formatStrategyNameWithSmallParentheses(name) {
